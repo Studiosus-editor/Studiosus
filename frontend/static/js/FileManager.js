3e2683039696be85ecef17896878e4f328ff728a
Petkevičius Lukas <lukas.petkevicius@mif.stud.vu.lt>
2024-03-07 23:17:58 +0200

    Feature/16 Add multi page support

--- frontend/static/js/FileManager.js
+++ frontend/static/js/FileManager.js
@@ -1,12 +1,49 @@
 class FileManager {
-  constructor() {}
-
-  loadFromLocalStorage(file = "file1") {
-    const storedFiles = localStorage.getItem(file);
-    return storedFiles ? JSON.parse(storedFiles) : [];
+  constructor() {
+    this.filePrefix = "code-editor-file-";
   }
 
-  saveToLocalStorage(content, file = "file1") {
-    localStorage.setItem(file, JSON.stringify(content));
+  loadFromLocalStorage(file) {
+    const storedFile = localStorage.getItem(this.filePrefix + file);
+    const fileData = storedFile ? JSON.parse(storedFile) : null;
+    return Array.isArray(fileData?.content) ? fileData.content : [];
+  }
+
+  saveToLocalStorage(content, file) {
+    const fileData = {
+      name: file,
+      content: content,
+      timestamp: new Date().getTime(), // Current time
+    };
+    localStorage.setItem(this.filePrefix + file, JSON.stringify(fileData));
+  }
+
+  getAllFiles() {
+    const files = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key.startsWith(this.filePrefix)) {
+        const fileData = JSON.parse(localStorage.getItem(key));
+        files.push(fileData);
+      }
+    }
+    // Sort files by timestamp
+    files.sort((a, b) => a.timestamp - b.timestamp);
+    // Return file names
+    return files.map((file) => file.name);
+  }
+
+  deleteFile(file) {
+    localStorage.removeItem(this.filePrefix + file);
+  }
+
+  // store currently Highlighted file in local storage
+  saveCurrentFile(file) {
+    localStorage.setItem("currentFile", file);
+  }
+
+  // get currently Highlighted file from local storage
+  getCurrentFile() {
+    return localStorage.getItem("currentFile");
   }
 }
