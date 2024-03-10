8031b6d9df2b973058b0fb77dbb46f2e1217a9b9
Maciūnas Adrijanas Juozas <adrijanas.maciunas@mif.stud.vu.lt>
2024-03-10 14:25:54 +0000

    Integrate sass support(#30)

--- frontend/src/Components/Navbar.svelte
+++ frontend/src/Components/Navbar.svelte
@@ -2,18 +2,19 @@
   <p>This is Navbar component</p>
 </nav>
 
-<style>
+<style lang="scss">
   nav {
     widows: 100%;
     height: 50px;
     background-color: var(--navbar-color);
+    
+    p {
+      text-align: center;
+      font-weight: bold;
+      color: white;
+      font-family: "Courier New", Courier, monospace;
+      padding-top: 15px;
+    }
   }
 
-  nav p {
-    text-align: center;
-    font-weight: bold;
-    color: white;
-    font-family: "Courier New", Courier, monospace;
-    padding-top: 15px;
-  }
 </style>
